2563e3141d216a645161610cbdd1b48d6456438e
mehdiakdim9 <87204966+mehdiakdim@users.noreply.github.com>
2024-01-09 15:58:55 +0100

    Add files via upload

---
+++ SpringSecurity-main/SpringSecurity-main/springsecsecurity/src/main/java/com/xproce/config/BankUserDetails.java
@@ -0,0 +1,8 @@
+package com.xproce.config;
+
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BankUserDetails implements UserDetailsService {
+}
